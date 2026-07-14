c52585aacd84fe042633fbb476d3b5487b80fe6c
Chrys12-art <ebrottiejunior123@gmail.com>
2026-07-14 09:20:47 +0000

    Ajout de la PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
 
 import App from "./App";
 
@@ -9,18 +10,17 @@ import "./styles/global.css";
 
 import { ThemeProvider } from "./context/ThemeContext";
 
+// Enregistre automatiquement le Service Worker de la PWA
+registerSW({
+  immediate: true,
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <ThemeProvider>
-
       <BrowserRouter>
-
         <App />
-
       </BrowserRouter>
-
     </ThemeProvider>
-
   </React.StrictMode>
 );
